4ed4f20c1de2b4c58849be226fcb907a772f4550
Nicolas <ni.rovegno@duocuc.cl>
2026-06-30 12:29:48 -0400

    Avance JWT y seguridad ms-suscripciones

--- ms-suscripciones/src/main/java/cl/teatromoro/suscripciones/exception/SuscripcionesExceptionHandler.java
+++ ms-suscripciones/src/main/java/cl/teatromoro/suscripciones/exception/SuscripcionesExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class SuscripcionesExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> manejarNotFound(
